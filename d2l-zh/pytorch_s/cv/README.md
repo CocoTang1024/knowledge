0f62d35ad337fb62b5edc0a18ce93c5252eaf356
Cocotang1024 <1972555958@qq.com>
2025-04-10 21:48:47 +0800

    看到torch网格部分的内容。

---
+++ d2l-zh/pytorch_s/cv/README.md
@@ -0,0 +1,1 @@
+from /home/t1122-5000/Codes/knowledge/d2l-zh/pytorch/chapter_computer-vision
---
+++ d2l-zh/pytorch_s/cv/anchor.py
@@ -0,0 +1,6 @@
+from d2l import torch as d2l
+import cv2
+import matplotlib.pyplot as plt
+cv2.imread()
+img = d2l.plt.imread("pytorch_s/img/catdog.jpg")
+img.shape
